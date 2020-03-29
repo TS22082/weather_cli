0ea1c0571a4832485398488686019082bfe9ca83
TS22082 <ts22082@gmail.com>
2020-03-29 15:21:06 -0700

    clean up request and get zip code from user

--- app.js
+++ app.js
@@ -2,21 +2,28 @@ const axios = require("axios");
 const inquirer = require("inquirer");
 require("dotenv").config();
 
-const zipUrl = `https://api.openweathermap.org/data/2.5/weather?zip=95616,us&appid=${process.env.WEATHER_KEY}`;
+const url = "https://api.openweathermap.org/data/2.5/weather";
 
 inquirer
   .prompt([
     {
-      type: "number",
-      message: "Enter a zip code\n",
+      type: "input",
+      message: "Enter a zip code or city, state\n",
       name: "userInput"
     }
   ])
   .then(inquirerRes => {
     if (typeof parseInt(inquirerRes.userInput[0]) != NaN) {
-      axios.get(zipUrl).then(res => {
-        console.log(res.data);
-      });
+      const finalUrl = `${url}?zip=${inquirerRes.userInput},us&units=imperial&appid=${process.env.WEATHER_KEY}`;
+      axios
+        .get(finalUrl)
+        .then(res => {
+          const data = res.data;
+          console.log(data);
+        })
+        .catch(() => {
+          console.log("No weather information found for this zipcode");
+        });
     }
   })
   .catch(() => {
