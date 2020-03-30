2f662caee637ba6524cda1c207a023a5f6551e33
GeekCentric <ts22082@gmail.com>
2020-03-29 17:14:56 -0700

    add options menu

--- app.js
+++ app.js
@@ -1,51 +1,72 @@
-const axios = require("axios");
-const inquirer = require("inquirer");
-require("dotenv").config();
+const axios = require('axios');
+const inquirer = require('inquirer');
+require('dotenv').config();
 
-const url = "https://api.openweathermap.org/data/2.5/weather";
+const url = 'https://api.openweathermap.org/data/2.5/weather';
 
-inquirer
-  .prompt([
-    {
-      type: "input",
-      message: "Enter a zip code or city, state\n",
-      name: "userInput"
-    }
-  ])
-  .then(inquirerRes => {
-    if (typeof parseInt(inquirerRes.userInput[0]) != NaN) {
-      const finalUrl = `${url}?zip=${inquirerRes.userInput},us&units=imperial&appid=${process.env.WEATHER_KEY}`;
-      axios
-        .get(finalUrl)
-        .then(res => {
-          const data = res.data;
-          console.log(data);
+function mainMenu() {
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: ['Get Weather', 'Quit'],
+        name: 'menuChoice'
+      }
+    ])
+    .then(inquirerResponse => {
+      if (inquirerResponse.menuChoice === 'Get Weather') {
+        getZip();
+      } else {
+        process.exit(0);
+      }
+    });
+}
+function getZip() {
+  inquirer
+    .prompt([
+      {
+        type: 'number',
+        message: 'Enter a zip code',
+        name: 'userInput'
+      }
+    ])
+    .then(inquirerRes => {
+      if (typeof parseInt(inquirerRes.userInput[0]) != NaN) {
+        const finalUrl = `${url}?zip=${inquirerRes.userInput},us&units=imperial&appid=${process.env.WEATHER_KEY}`;
+        axios
+          .get(finalUrl)
+          .then(res => {
+            const data = res.data;
 
-          console.log("--------------------");
-
-          const tableData = {};
-          for (item in data.main) {
-            switch (item) {
-              case "pressure":
-                tableData[item] = data.main[item] + "mbar";
-                break;
-              case "humidity":
-                tableData[item] = data.main[item] + "ah";
-                break;
-              default:
-                tableData[item] = data.main[item] + "F°";
-                break;
+            const tableData = {};
+            for (item in data.main) {
+              switch (item) {
+                case 'pressure':
+                  tableData[item] = data.main[item] + 'mbar';
+                  break;
+                case 'humidity':
+                  tableData[item] = data.main[item] + 'ah';
+                  break;
+                default:
+                  tableData[item] = data.main[item] + 'F°';
+                  break;
+              }
             }
-          }
 
-          console.table(tableData);
-        })
-        .catch(() => {
-          console.log("No weather information found for this zipcode");
-        });
-    }
-  })
-  .catch(() => {
-    console.log("There was a fatal mistake");
-    process.exit();
-  });
+            console.table(tableData);
+            mainMenu();
+          })
+          .catch(() => {
+            console.log('No weather information found for this zipcode');
+            mainMenu();
+          });
+      }
+    })
+    .catch(() => {
+      console.log('There was a fatal mistake');
+      process.exit();
+    });
+}
+
+mainMenu();
